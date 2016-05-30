6c1f50c29faf3096050ffcfeadf0c051d532ea7b
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-05-31 00:25:35 +0200

    add definition of missing macros like F_DUPFD & Co

--- newlib/libc/sys/hermit/sys/fcntl.h
+++ newlib/libc/sys/hermit/sys/fcntl.h
@@ -64,6 +64,17 @@
 #define O_CLOEXEC	02000000	/* set close_on_exec */
 #endif
 
+#define F_DUPFD		0	/* dup */
+#define F_GETFD		1	/* get close_on_exec */
+#define F_SETFD		2	/* set/clear close_on_exec */
+#define F_GETFL		3	/* get file->f_flags */
+#define F_SETFL		4	/* set file->f_flags */
+#ifndef F_GETLK
+#define F_GETLK		5
+#define F_SETLK		6
+#define F_SETLKW	7
+#endif
+
 /* --- redundant stuff below --- */
 
 #include <_ansi.h>
