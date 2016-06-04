b19a35ae9f5b162ce9c5a13c41694cfb1500db16
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-06-04 07:39:42 +0200

    remove wrong declaration of pthread_sigmask

--- newlib/libc/include/sys/signal.h
+++ newlib/libc/include/sys/signal.h
@@ -185,7 +185,6 @@ int _EXFUN(kill, (pid_t, int));
 #endif /* __CYGWIN__ || __rtems__ || __SPU__ */
 #ifdef defined(__hermit__)
 int _EXFUN(sigaction, (int, const struct sigaction *, struct sigaction *));
-#define pthread_sigmask sigaction
 #endif
 #if defined(__CYGWIN__) || defined(__rtems__)
 int _EXFUN(killpg, (pid_t, int));
