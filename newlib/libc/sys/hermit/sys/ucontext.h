30a3e67a8641c3a6884ad3d18e1fc8752652e89c
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-09-10 16:43:04 +0200

    save /restore only MXCSR & the x87 status word

--- newlib/libc/sys/hermit/sys/ucontext.h
+++ newlib/libc/sys/hermit/sys/ucontext.h
@@ -18,68 +18,6 @@
 extern "C" {
 #endif
 
-typedef struct {
-	uint32_t cwd;
-	uint32_t swd;
-	uint32_t twd;
-	uint32_t fip;
-	uint32_t fcs;
-	uint32_t foo;
-	uint32_t fos;
-	uint32_t st_space[20];
-	uint32_t status;
-} i387_fsave_t;
-
-typedef struct {
-	uint16_t cwd;
-	uint16_t swd;
-	uint16_t twd;
-	uint16_t fop;
-	union {
-		struct {
-			uint64_t rip;
-			uint64_t rdp;
-		};
-		struct {
-			uint32_t fip;
-			uint32_t fcs;
-			uint32_t foo;
-			uint32_t fos;
-		};
-	};
-	uint32_t mxcsr;
-	uint32_t mxcsr_mask;
-	uint32_t st_space[32];
-	uint32_t xmm_space[64];
-	uint32_t padding[12];
-	union {
-		uint32_t padding1[12];
-		uint32_t sw_reserved[12];
-	};
-} i387_fxsave_t __attribute__ ((aligned (16)));
-
-typedef struct {
-	uint64_t xstate_bv;
-	uint64_t xcomp_bv;
-	uint64_t reserved[6];
-} xsave_header_t;
-
-typedef struct {
-	uint32_t ymmh_space[64];
-} ymmh_t;
-
-typedef struct {
-	i387_fxsave_t fxsave;
-	xsave_header_t hdr;
-	ymmh_t ymmh;
-} xsave_t __attribute__ ((aligned (64)));
-
-union fpu_state {
-	i387_fsave_t    fsave;
-	i387_fxsave_t   fxsave;
-	xsave_t xsave;
-};
-
 typedef struct mregs {
 	uint64_t r15;
 	uint64_t r14;
@@ -95,11 +33,28 @@ typedef struct mregs {
 	uint64_t rcx;
 	uint64_t rsp;
 	uint64_t rip;
+	uint32_t mxcsr;
 } mregs_t;
+
+typedef struct {
+        uint16_t control_word;
+        uint16_t unused1;
+        uint16_t status_word;
+        uint16_t unused2;
+        uint16_t tags;
+        uint16_t unused3;
+        uint32_t eip;
+        uint16_t cs_selector;
+        uint32_t opcode:11;
+        uint32_t unused4:5;
+        uint32_t data_offset;
+        uint16_t data_selector;
+        uint16_t unused5;
+} fenv_t;
 
 typedef struct ucontext {
 	mregs_t		uc_mregs;
-	union fpu_state	uc_fpu;
+	fenv_t		uc_fenv;
 	struct ucontext	*uc_link;
 	stack_t		uc_stack;
 } ucontext_t;
