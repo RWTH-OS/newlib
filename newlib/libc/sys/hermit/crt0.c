7fe6212bf8af2e6a0c16792cf563007cd9c18069
Stefan Lankes <stlankes@users.noreply.github.com>
2018-04-28 18:57:16 +0200

    add draft of aarch64 port

--- newlib/libc/sys/hermit/crt0.c
+++ newlib/libc/sys/hermit/crt0.c
@@ -69,6 +69,7 @@ int libc_start(int argc, char** argv, char** env)
    return 0;
 }
 
+#ifdef __x86_64__
 int get_cpufreq(void)
 {
    return *((int*) (PHYS + 0x18));
@@ -83,3 +84,9 @@ int get_num_cpus(void)
 {
    return *((int*) (PHYS + 0x20));
 }
+#elif defined(__x86_64__)
+int get_num_cpus(void)
+{
+   return *((int*) (PHYS + 0x120));
+}
+#endif
